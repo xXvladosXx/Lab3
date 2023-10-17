f0ae66fe343d6e1732f48d09a5f8bc7ef514f536
Mateusz Redestowicz <mati.red@gmail.com>
2023-10-17 11:34:56 +0200

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
